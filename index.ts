fafd5e79c1a7887edf0317ec03db934867f30f7f
Muhiba Asif <nabihanabihaasif@gmail.com>
2024-12-29 00:03:13 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import  inquirer from "inquirer";
+let MyBalance = 2000
+let MyPin = 1234;
+
+let myanswer = await inquirer.prompt([
+    {
+        message="pin your code "
+    }
+])
+     
+    
+        
+            
+    
+    
+    
